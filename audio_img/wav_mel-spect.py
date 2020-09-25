ae4704ef826ce3e23a2e7cc83f56b97d860b822a
kanat3 <30739653+kanat3@users.noreply.github.com>
2020-09-26 01:00:52 +0300

    Create wav_mel-spect.py

---
+++ audio_img/wav_mel-spect.py
@@ -0,0 +1,26 @@
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+y, sr = librosa.load('Documents/common_voice_ru_18849003.wav')
+plt.plot(y);
+plt.title('Signal');
+plt.xlabel('Time (samples)');
+plt.ylabel('Amplitude');
+n_fft = 2048
+ft = np.abs(librosa.stft(y[:n_fft], hop_length = n_fft+1))
+plt.plot(ft);
+plt.title('Spectrum');
+plt.xlabel('Frequency Bin');
+plt.ylabel('Amplitude');
+spec = np.abs(librosa.stft(y, hop_length=512))
+spec = librosa.amplitude_to_db(spec, ref=np.max)
+librosa.display.specshow(spec, sr=sr, x_axis='time', y_axis='log');
+plt.colorbar(format='%+2.0f dB');
+plt.title('Spectrogram');
+mel_spect = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=2048, hop_length=1024)
+mel_spect = librosa.power_to_db(spect, ref=np.max)
+librosa.display.specshow(mel_spect, y_axis='mel', fmax=8000, x_axis='time');
+plt.title('Mel Spectrogram');
+plt.colorbar(format='%+2.0f dB');
