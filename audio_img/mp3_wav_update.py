15f4dad90b3fad0f12365af404fa199263e7cae8
kanat3 <30739653+kanat3@users.noreply.github.com>
2020-10-04 19:44:30 +0300

    Update mp3_wav_update.py

--- audio_img/mp3_wav_update.py
+++ audio_img/mp3_wav_update.py
@@ -3,7 +3,6 @@ from pydub import AudioSegment
 #загружаем все имена файлов в files формата 'name.mp3'
 directory = 'C:/Users/localadmin/Desktop/clips'
 files = os.listdir(directory)
-print(files)
 for k in range (0,76565):
   src = "{}{}{}".format("C:/Users/localadmin/Desktop/clips/",files[k], ".mp3")
   dst = "{}{}{}".format("C:/Users/localadmin/Desktop/test/",files[k], ".wav")
