d96496d9a0efec88f15c8983a532b67081fccf2f
kanat3 <30739653+kanat3@users.noreply.github.com>
2020-10-08 14:19:21 +0300

    Update wav_mel-spect.py

--- audio_img/wav_mel-spect.py
+++ audio_img/wav_mel-spect.py
@@ -1,4 +1,7 @@
-#для одного файла, вывод без шкал и белого окна
+# сохраняет спектограмму n-файла из папки с wav-аудио
+import os
+from os import path
+from pydub import AudioSegment
 import librosa
 import librosa.display
 import numpy as np
@@ -6,22 +9,30 @@ import matplotlib.pyplot as plt
 import pylab
 from PIL import Image
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-#показывает mel-спектограмму для одного файла
-filename = r'D:\test\common_voice_ru_18849119.wav'
-y, sr = librosa.load(filename)
-# trim silent edges
-whale_song, _ = librosa.effects.trim(y)
-librosa.display.waveplot(whale_song, sr=sr);
-n_fft = 2048
-D = np.abs(librosa.stft(whale_song[:n_fft], n_fft=n_fft, hop_length=n_fft+1))
-hop_length = 256
-D = np.abs(librosa.stft(whale_song, n_fft=n_fft, hop_length=hop_length))
-librosa.display.specshow(D, sr=sr, y_axis='linear');
-DB = librosa.amplitude_to_db(D, ref=np.max)
-librosa.display.specshow(DB, sr=sr, hop_length=hop_length, y_axis='log');
-plt.gca().set_axis_off()
-plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+#загружаем все имена файлов в files формата 'name.mp3'
+directory = 'D:/test'
+files = os.listdir(directory)
+for k in range (0,76564):
+    filename = "{}{}".format("D:/test/",files[k])
+    y, sr = librosa.load(filename)
+    fig, ax = plt.subplots()
+    # trim silent edges
+    whale_song, _ = librosa.effects.trim(y)
+    librosa.display.waveplot(whale_song, sr=sr);
+    n_fft = 2048
+    D = np.abs(librosa.stft(whale_song[:n_fft], n_fft=n_fft, hop_length=n_fft+1))
+    hop_length = 256
+    D = np.abs(librosa.stft(whale_song, n_fft=n_fft, hop_length=hop_length))
+    librosa.display.specshow(D, sr=sr, y_axis='linear');
+    DB = librosa.amplitude_to_db(D, ref=np.max)
+    librosa.display.specshow(DB, sr=sr, hop_length=hop_length, y_axis='log');
+    pylab.axis('off')
+    plt.gca().set_axis_off()
+    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
             hspace = 0, wspace = 0)
-plt.margins(0,0)
-plt.gca().xaxis.set_major_locator(plt.NullLocator())
-plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.margins(0,0)
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    save_path = "{}{}{}".format('D:/image/', files[k], '.png')
+    plt.savefig(save_path)
+    plt.clf()
